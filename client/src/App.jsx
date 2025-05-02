81d9cfb1481da4f5af3e6631d20b822dfd32eabf
sagardubey14 <sagardubey353@gmail.com>
2025-05-02 14:38:17 +0530

    Auth UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import Auth from "./Components/Auth";
+import { Route, Routes } from 'react-router-dom';
+
+
 function App() {
   return (
-    <div className="text-3xl font-bold underline text-blue-400">
-      Flock-List
+    <div className="bg-gray-100 min-h-screen">
+      <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+        <div className="mb-4 mt-16 text-3xl md:text-4xl font-bold underline text-blue-400 text-center">
+          Flock-List
+        </div>
+        <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/about" element={<Auth />} />
+        <Route path="/*" element={<Auth />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
